a7f96528698d498f10f01cbdb5629917607ef76d
kruemmelbande <kai.scheufen@gmail.com>
2023-08-11 08:09:53 +0200

    made the count not hardcoded anyore

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::time::{Duration, Instant};
 use image::GenericImageView;
-
+use std::fs;
 
 fn main() {
-    let n = 33; // loop every 1000 milliseconds
+    let n = 0; // loop every n milliseconds
+    let folder_path = "video/";
+    let f:usize=fs::read_dir(folder_path).expect("Failed to read folder.").count();
     let mut last_time = Instant::now();
-    for frame in 1..6571{
+    for frame in 1..f{
 
         // Open the image file
         let path: String=format!("video/apple-{:0width$}.png",frame,width=5);
@@ -28,11 +30,16 @@ fn main() {
                 // Do something with the RGB values
                 //println!("Pixel at ({}, {}) has RGB values ({}, {}, {})", x, y, r, g, b);
                 let pixel_bw:u8=((r as i16+b as i16+g as i16) as i16/3 as i16) as u8;
-                if pixel_bw<127{
-                    print!(" ");
-                }else{
-                    print!("#");
+                match pixel_bw {
+                    0..=42 => print!(" "),
+                    43..=84 => print!("."),
+                    85..=126 => print!("-"),
+                    127..=168 => print!("="),
+                    169..=210 => print!("+"),
+                    211..=252 => print!("*"),
+                    253..=255 => print!("#")
                 }
+                
             }
             println!();
         }
