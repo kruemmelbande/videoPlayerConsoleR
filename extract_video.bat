9c160e721f87c16b95c58b6520e3bae604a7e0fb
kruemmelbande <kai.scheufen@gmail.com>
2023-08-14 22:56:00 +0200

    added extractor scripts, and stated dependencies

---
+++ extract_video.bat
@@ -0,0 +1,32 @@
+@echo off
+
+REM Check if an input video file is provided as an argument
+if "%~1"=="" (
+    echo Usage: %~nx0 ^<input_video^>
+    exit /b 1
+)
+
+REM Input video file provided as argument
+set "input_video=%~1"
+
+echo Input video: %input_video%
+
+REM Create the 'video' directory or clear it if already exists
+if exist "video" (
+    
+    echo 'video' directory already exists. Because batch hates me, I cant ask you if i should clear it for you so.... pls delete it and try again, ty
+    exit /b 1
+) else (
+    mkdir "video"
+)
+
+REM Extract frames from the input video
+@echo on
+ffmpeg -i "%input_video%" "video\frame%%05d.jpg"
+@echo off
+REM Extract audio from the input video and save as 'audio.mp3'
+@echo on
+ffmpeg -i "%input_video%" -vn -acodec libmp3lame "audio.mp3"
+@echo off
+echo Frames extracted to 'video' directory, and audio saved as 'audio.mp3'
+
